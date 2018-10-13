089959d9aacb0df4522a1ada68eb434109b22ae3
Dinaol Tadesse <dinaolmelak@gmail.com>
2018-10-13 15:00:30 -0500

    Initial Commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Dinaol Melak on 10/13/18.
+//  Copyright © 2018 Dinaol Melak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
